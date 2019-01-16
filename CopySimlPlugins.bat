7b95acbc0a383467b93d49bca7c58e78e3278fb4
Christ Prateek Prasanna Xaxa <cppxaxa@live.com>
2019-01-16 22:00:31 +0530

    first commit

---
+++ CopySimlPlugins.bat
@@ -0,0 +1,3 @@
+xcopy ICAN.SIC.Plugin.SIMLHub.Plugins\SIMLHub_Test\bin\Debug\Plugins\SIMLHub\SIMLHubPlugins "ICAN.SIC_Executables - Copy\Plugins\SIMLHub\SIMLHubPlugins\" /e /y
+xcopy ICAN.SIC.Plugin.SIMLHub.Plugins\SIMLHub_Test\bin\Debug\Plugins\SIMLHub\SIMLHubPlugins ICAN.SIC_Executables\Plugins\SIMLHub\SIMLHubPlugins\ /e /y
+xcopy ICAN.SIC.Plugin.SIMLHub.Plugins\SIMLHub_Test\bin\Debug\Plugins\SIMLHub\SIMLHubPlugins ICAN.SIC\bin\Debug\SIMLHubPlugins\ /e /y
---
+++ ICANSourceDownloader.sh
@@ -0,0 +1,9 @@
+git clone https://github.com/cppxaxa/ICAN.SIC
+git clone https://github.com/cppxaxa/ICAN.SIC.Plugin.ChatInterfaceWeb
+git clone https://github.com/cppxaxa/ICAN.SIC.Plugin.ICANCOMPREHEND
+git clone https://github.com/cppxaxa/ICAN.SIC.Plugin.ICANSEE
+git clone https://github.com/cppxaxa/ICAN.SIC.Plugin.SIMLHub.Plugins
+git clone https://github.com/cppxaxa/ICAN.SIC_Executables
+git clone https://github.com/cppxaxa/ICANDevelopmentContents
+git clone https://github.com/cppxaxa/PyTaskExecuterService
+
